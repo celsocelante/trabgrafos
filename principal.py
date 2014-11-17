3e96e6170d544288a10f63e621f20213f2750bc4
Celso Junior <celsojr100@gmail.com>
2014-11-17 14:50:16 -0200

    Construindo classe Grafo

---
+++ principal.py
@@ -0,0 +1,10 @@
+class Grafo;
+    def __init__(self):
+        self.lista_vertices = []
+        self.lista_arestas = []
+
+    def add_vertice(self,vertice):
+        lista_vertices.append(vertice)
+
+    def add_aresta(self,aresta):
+        lista_arestas.append(aresta)
