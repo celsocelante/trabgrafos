57582181e12c3609fc642b3d5977681d785b86ad
Marcos Prado <mprado@inf.ufes.br>
2014-11-23 22:31:38 -0200

    Adicionadas pequenas modificações

--- grafo.py
+++ grafo.py
@@ -1,15 +1,17 @@
 class Grafo:
-    lista_vertices = []
-    lista_arestas = []
-    lista_arestas_pesos = []
-
     def __init__(self,n,m):
-        n_vertices = n
-        n_arestas = m
-
+        self.n=n
+        self.m=m
+        self.lista_vertices = []
+        self.lista_arestas = []
+        self.lista_arestas_pesos = []
     def add_vertice(self,vertice):
-        lista_vertices.append(vertice)
+        if not vertice in self.lista_vertices:
+            self.lista_vertices.append(vertice)
 
     def add_aresta(self,aresta,peso):
-        lista_arestas.append(aresta)
-        lista_arestas_pesos.append(peso)
+        self.lista_arestas.append(aresta)
+        self.lista_arestas_pesos.append(peso)
+
+    def __str__(self):
+        return 'Arestas: ' + ','.join(map(str,self.lista_arestas)) + '\nPesos: ' + ','.join(map(str,self.lista_arestas_pesos))
--- principal.py
+++ principal.py
@@ -1,6 +1,8 @@
 from grafo import Grafo
 f = open('entrada.txt', 'r')
 
+lista_grafos = []
+j=0;
 line = f.readline().rstrip()
 while line != '':
 
@@ -9,21 +11,25 @@ while line != '':
     source = int(line[4])
     target = int(line[6])
 
-    print n
-    print m
-    print source
-    print target
+#    print n
+#    print m
+#    print source
+#    print target
 
-    #g = Grafo(n,m)
+    lista_grafos.append(Grafo(n,m))
 
     for i in range(m):
         string = f.readline().rstrip()
         a1 = int(string[0])
         a2 = int(string[2])
-        peso = int(string[4])
+        peso = int(string[4:])
 
-        #g.add_aresta((a1,a2),peso)
-        #g.add_vertice((a1,a2))
+        lista_grafos[j].add_aresta((a1,a2),peso)
+        lista_grafos[j].add_vertice(a1)
+        lista_grafos[j].add_vertice(a2)
+
+    print lista_grafos[j]
+    j+=1
 
     line = f.readline().rstrip() # linha em branco
     line = f.readline().rstrip()
